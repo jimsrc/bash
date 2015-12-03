c206623e26a464aa44a8e46f8ff4e7291b5a54b5
jimsrc <jimmy.ilws@gmail.com>
2015-12-03 20:46:40 -0300

    fix rsync

--- sincronizar_TODO_at_forbush.sh
+++ sincronizar_TODO_at_forbush.sh
@@ -33,7 +33,7 @@ HDDname='Elements'                                  # nombre de mi hdd externo
 ROOT_SRC=$HOME                                      # directorio raiz local
 ROOT_DST=/media/$HDDname/oficina                    # directorio raiz destino
 #+++ agrega directorio de backup, y sufijo a c/archivo backupeado
-backup_arg="--backup-dir=\"bckp\" --suffix=\"_bckp_`hostname`_`date +%d%b%Y_%H.%M.%S`\""  # back up argument
+backup_arg="--backup-dir=bckp --suffix=_bckp_`hostname`_`date +%d%b%Y_%H.%M.%S`"  # back up argument
 other_arg='-rvubthl --human-readable'               # general args
 RSYNC="/usr/bin/rsync"                              # binario del sistema
 #
