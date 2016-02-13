a26185c3fa453272cb7ff76773281034b61d9dbf
jimsrc <jimmy.ilws@gmail.com>
2016-02-13 19:21:46 -0300

    'tt2.sh' now catches Ctrl-C gracefully :-)

--- try_catch/tt2.sh
+++ try_catch/tt2.sh
@@ -4,18 +4,15 @@ trap "pressed_ctrl_c=1" INT
 while true
 do
     (trap "" INT; 
-    #echo "running..."; 
     sleep 0.2)&
     wait  #||  wait
     if [ "$pressed_ctrl_c" ]; then
         echo " ---> KEYBOARD INTERR!"
-        #continue #break
         read -p ' Continue? (y/[n]): ' YN && echo #" -answer:"$?
         if [ "${YN}" == 'y' ]; then
             pressed_ctrl_c= # forget i ever pressed ctrl-c
             continue
         else
-            #echo " --> YN:"$YN
             echo " ---> Exit gracefully..."
             exit 0 #break #exit 0
         fi
@@ -23,5 +20,5 @@ do
     else
         echo " running..."
     fi
-    #echo " THIS SHOULD NOT BE SEEN"
 done
+exit 0
